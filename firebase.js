e4fa6886aabb8379a29c3a4e5d2bbd856fcfd815
kshitij-garg18 <kshitijgarg484@gmail.com>
2024-04-25 15:59:03 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore , collection } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCs4-rp67npoUJJ8GLrK40VIkXkPgjNMxc",
-  authDomain: "notes--react-448e0.firebaseapp.com",
-  projectId: "notes--react-448e0",
-  storageBucket: "notes--react-448e0.appspot.com",
-  messagingSenderId: "1042873585068",
-  appId: "1:1042873585068:web:1dbf2e38c98ccf3953fe09",
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id"
 };
 
 
